8ad7a771eda8ee19048e0a413f5327fa01a397ba
anatomij <artjom.svjatickis@gmail.com>
2016-04-05 04:51:43 -0700

    Refactoring express for heroku

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+
 app.use(express.static(__dirname + '/../client/'));
+
+app.get('/', function(request, response) {
+  response.render('../client/blackjack.html')
+});
 
 app.listen(process.env.PORT || 3000);
